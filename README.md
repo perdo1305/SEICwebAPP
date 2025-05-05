56677bd40aa609b01e18a54fbf97dea693babd64
Pedro Ferreira <88007090+perdo1305@users.noreply.github.com>
2025-05-05 11:33:00 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Red-Node Azure Web app 
+
+Base code for setting up a Node-Red service on Azure.
+
+This code was developed based on the https://github.com/Azure-Samples/nodejs-docs-hello-world/ from Microsoft Azure development team.
+
+Developed for SEIC, LEEC, ESTG@IPLeiria, Portugal.
+
---
+++ index.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
+
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object
+var settings = require("./settings.js")
+
+// Initialise the runtime with a server and settings 
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(process.env.port);
+console.log(`listening port:${settings.uiPort}`);
+
+// Start the runtime
+RED.start();
+
